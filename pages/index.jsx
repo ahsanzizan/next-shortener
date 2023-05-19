6ff27e52e61e5dcf067142c2783ab09ecc6d9d9a
Ahsan <ahsanaz461@gmail.com>
2023-05-19 13:19:28 +0700

    add not found page

--- pages/index.jsx
+++ pages/index.jsx
@@ -46,13 +46,13 @@ export default function Home({ data }) {
       <div className="w-full max-w-xs mx-auto">
         <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={saveChanges}>
           <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" for="username">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="fullUrl">
               Full URL
             </label>
             <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="url" name="fullUrl" placeholder="Enter the Full URL" autoComplete="off" required />
           </div>
           <div className="mb-6">
-            <label className="block text-gray-700 text-sm font-bold mb-2" for="password">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="shortUrl">
               Short URL
             </label>
             <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" type="text" name="shortUrl" placeholder="Enter the Short URL You Want" required />
